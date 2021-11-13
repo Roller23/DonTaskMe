80cfb87b247e84d75abb8a256b09ce39f254bbe1
Wiktor Kania <roller47@wp.pl>
2021-11-13 15:10:50 +0100

    fancy css voodoo magic

--- src/components/WorkspacesLayer.vue
+++ src/components/WorkspacesLayer.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="layer">
-    <h1>{{workspacesText}}</h1>
-    <div class="workspaces">
-      <div v-for="workspace in workspaces"
-           v-bind:key="workspace.title" class="workspace-wrap workspace"
-           :class="{hidden: workspace.hidden}"
-           @click="workspaceSelect(workspace)"
-      >
-        <h2>{{workspace.title}}</h2>
-        <p>{{workspace.desc}}</p>
+    <h1 class="top-text">{{workspacesText}}</h1>
+    <div class="boxes-wrap">
+      <div class="workspaces" :class="{hidden: currentWorkspace !== null}">
+        <div class="workspace-wrap new-workspace">
+          Create a new workspace
+        </div>
+        <div v-for="workspace in workspaces"
+            v-bind:key="workspace.title" class="workspace-wrap workspace"
+            :class="{hidden: workspace.hidden}"
+            @click="workspaceSelect(workspace)">
+          <h2 class="title">{{workspace.title}}</h2>
+          <p class="desc">{{workspace.desc}}</p>
+        </div>
       </div>
-      <div class="workspace-wrap new-workspace">
-        Create a new workspace
+      <div class="boards" :class="{expanded: currentWorkspace !== null}">
+        <transition name="fade">
+          <div class="chooseWorkspace" v-if="currentWorkspace === null">
+            <img class="icon" src="@/assets/workspace.png" alt="Workspace icon">
+            <h2 class="text">Choose a workspace to see its boards</h2>
+          </div>
+        </transition>
       </div>
     </div>
   </div>
@@ -48,6 +57,7 @@ export default {
           workspace.hidden = false;
         }
         this.workspacesText = 'Your workspaces';
+        this.currentWorkspace = null;
         return;
       }
       this.workspacesText = selectedWorkspace.title;
@@ -69,12 +79,54 @@ export default {
   height: 100%;
   background-color: rgb(233, 233, 233);
   padding: 20px;
+  padding-bottom: 0px;
+}
+.top-text {
+  height: 50px;
+  margin: 0px;
+}
+.boxes-wrap {
+  height: calc(100% - 50px);
+  white-space: nowrap;
 }
 .workspaces {
   width: 50%;
   height: 100%;
   overflow: auto;
   padding-right: 5px;
+  transition: 0.6s;
+  display: inline-block;
+}
+.workspaces.hidden {
+  transform: translateX(-60%);
+}
+.boards {
+  color: black;
+  display: inline-block;
+  vertical-align: top;
+  width: 50%;
+  padding: 20px;
+  height: 100%;
+  transition: 0.6s;
+  position: relative;
+}
+.boards.expanded {
+  transform: translateX(-37%);
+  width: 80%;
+}
+.chooseWorkspace {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+.chooseWorkspace .icon {
+  width: 300px;
+  filter: grayscale(20%)
+}
+.chooseWorkspace .text {
+  color: gray;
 }
 .workspace-wrap {
   cursor: pointer;
@@ -82,15 +134,26 @@ export default {
   background-color: white;
   box-shadow: 1px 1px 1px 1px rgb(172, 172, 172);
   margin-bottom: 10px;
-  border-radius: 10px;
+  border-radius: 5px;
   transition: 0.3s;
   text-align: right;
 }
 .workspace-wrap.hidden {
-  transform: translateX(-70%);
+  transform: translateX(-10%);
 }
 .workspace-wrap:hover {
   transform: scale(1.01) translate(-2px, -2px);
   box-shadow: 1px 1px 4px 2px rgb(172, 172, 172);
+}
+
+.workspace-wrap .desc {
+  color: gray;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
 }
 </style>

a618625bfb0526afe8d451dce7c93a7f468f8460
adacienciala <adacien@o2.pl>
2021-11-28 21:19:34 +0100

    Roughly done

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,45 @@
 <template>
-  <WorkspacesLayer></WorkspacesLayer>
-  <LoginLayer v-if="!hasToken()"></LoginLayer>
+	<WorkspacesLayer v-if="false"></WorkspacesLayer>
+	<LoginLayer v-if="false"></LoginLayer>
+	<BoardLayer></BoardLayer>
 </template>
 
 <script>
-import LoginLayer from './components/LoginLayer.vue'
-import WorkspacesLayer from './components/WorkspacesLayer.vue'
+import LoginLayer from "./components/LoginLayer.vue";
+import WorkspacesLayer from "./components/WorkspacesLayer.vue";
+import BoardLayer from "./components/BoardLayer/BoardLayer.vue";
 
 export default {
-  name: 'App',
-  components: {
-    LoginLayer, WorkspacesLayer
-  },
-  data() {
-    return {
-      hasToken() {
-        return !!localStorage.token;
-      }
-    }
-  }
-}
+	name: "App",
+	components: {
+		LoginLayer,
+		WorkspacesLayer,
+		BoardLayer,
+	},
+	data() {
+		return {
+			hasToken() {
+				return !!localStorage.token;
+			},
+		};
+	},
+};
 </script>
 
 <style>
-
 * {
-  box-sizing: border-box;
+	box-sizing: border-box;
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  position: fixed;
-  top: 0px;
-  left: 0px;
-  width: 100vw;
-  height: 100vh;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #2c3e50;
+	position: fixed;
+	top: 0px;
+	left: 0px;
+	width: 100vw;
+	height: 100vh;
 }
 </style>
